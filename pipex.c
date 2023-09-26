7a83e4a0e64906bae2470f102ce00f341c6bf7b5
iuri~ <123178126+iurilandolt@users.noreply.github.com>
2023-09-26 12:03:22 +0100

    testing libraries

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: rlandolt <rlandolt@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 11:38:41 by rlandolt          #+#    #+#             */
-/*   Updated: 2023/09/26 11:54:44 by rlandolt         ###   ########.fr       */
+/*   Updated: 2023/09/26 12:02:43 by rlandolt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 
 int	main(int argc, char **argv)
 {
-
+	int test = 420;
+	ft_printf("arg %d\n", test);
 }
