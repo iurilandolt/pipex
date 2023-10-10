cd0c8930f46c35b7fa0c2f752c139e116d7ba0de
iuri- <nemo@naut>
2023-10-10 13:20:08 +0100

    handling errors

--- README.md
+++ README.md
@@ -3,3 +3,79 @@ https://chat.openai.com/share/b9b84508-050d-4bca-8094-de3b4a56978e
 https://medium.com/@lannur-s/pipex-42-chapter-3-mastering-execve-using-fork-f93906a79d7c
 https://github.com/gabcollet/pipex/blob/master/srcs/pipex.c
 https://github.com/iurilandolt/pipex
+
+/*
+
+
+	void forkexample()
+{
+    int x = 1;
+
+    if (fork() == 0)
+        printf("Child has x = %d\n", ++x);
+    else
+        printf("Parent has x = %d\n", --x);
+}
+int main()
+{
+    forkexample();
+    return 0;
+}
+
+	(void)argc;
+	(void)argv;
+	for (int i = 0; envp[i] != NULL; i++)
+	{
+		printf("%s\n", envp[i]);
+	}
+
+	void	child_proc(char ** argv, char **envp, int *fd)
+{
+	int filein;
+
+	filein = open(argv[1], O_RDONLY, 0551);
+	if (filein == -1)
+	{
+		perror("Error:");
+		exit(1);
+	}
+	dup2(fd[1], STDOUT_FILENO);
+	dup2(filein, STDIN_FILENO);
+	close(fd[0]);
+	//execute
+}
+
+void	child_proc(int *fd, char **argv, char **envp)
+{
+	int		filein;
+	char	buffer[BUFFER_SIZE];
+	int		n;
+
+	filein = open(argv[1], O_RDONLY);
+	if (filein == -1)
+		ft_error();
+	close(fd[0]);
+	while ((n = read(filein, buffer, BUFFER_SIZE)) > 0)
+		write(fd[1], buffer, n);
+	close(filein);
+	close(fd[1]);
+	execute(argv[2], envp);
+}
+
+void parent_proc(int *fd, char **argv, char**envp)
+{
+	int		fileout;
+	char	buffer[BUFFER_SIZE];
+	int		n;
+
+	fileout = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fileout == -1)
+		ft_error();
+	close(fd[1]);
+	while ((n = read(fd[0], buffer, BUFFER_SIZE)) > 0)
+		write(fileout, buffer, n);
+	close(fileout);
+	close(fd[0]);
+	execute(argv[3], envp);
+}
+*/
--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: rlandolt <rlandolt@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 11:38:41 by rlandolt          #+#    #+#             */
-/*   Updated: 2023/10/10 11:19:18 by rlandolt         ###   ########.fr       */
+/*   Updated: 2023/10/10 13:13:35 by rlandolt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@ char	*find_path(char **envp, char *cmd)
 		free(path);
 		if (access(program, F_OK) == 0)
 		{
-			printf("cmd found at: %s\n", program);
 			clear(paths);
 			return (program);
 		}
@@ -48,44 +47,43 @@ void	execute(char *argv, char **envp)
 
 	cmd = ft_split(argv, ' ');
 	if (!cmd)
-		return ;
+		ft_error();
 	path = find_path(envp, *cmd);
-	execve(path, cmd, envp);
-	free(path);
-	clear(cmd);
+	if (!path)
+		ft_error();
+	if (execve(path, cmd, envp) == -1)
+	{
+		free(path);
+		clear(cmd);
+		ft_error();
+	}
 }
 
 void	child_proc(int *fd, char **argv, char **envp)
 {
 	int		filein;
-	char	buffer[BUFFER_SIZE];
-	int		n;
 
 	filein = open(argv[1], O_RDONLY);
 	if (filein == -1)
 		ft_error();
+	dup2(fd[1], STDOUT_FILENO);
 	close(fd[0]);
-	while ((n = read(filein, buffer, BUFFER_SIZE)) > 0)
-		write(fd[1], buffer, n);
-	close(filein);
-	close(fd[1]);
+	dup2(filein, STDIN_FILENO);
+	//close(filein);
 	execute(argv[2], envp);
 }
 
 void parent_proc(int *fd, char **argv, char**envp)
 {
 	int		fileout;
-	char	buffer[BUFFER_SIZE];
-	int		n;
 
 	fileout = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fileout == -1)
 		ft_error();
+	dup2(fd[0], STDIN_FILENO);
 	close(fd[1]);
-	while ((n = read(fd[0], buffer, BUFFER_SIZE)) > 0)
-		write(fileout, buffer, BUFFER_SIZE);
-	close(fileout);
-	close(fd[0]);
+	dup2(fileout, STDOUT_FILENO);
+	//close(fileout);
 	execute(argv[3], envp);
 }
 
@@ -98,56 +96,16 @@ int main(int argc, char **argv, char **envp)
 	if (argc == 5)
 	{
 		if (pipe(fd) == -1)
-			return (1);
+			ft_error();
 		proc_id = fork();
+		if (proc_id == -1)
+			ft_error();
 		if (proc_id == 0)
 			child_proc(fd, argv, envp);
-		waitpid(proc_id, NULL, 0);
-		parent_proc(fd, argv, envp);
+		else
+			parent_proc(fd, argv, envp);
 	}
 	else
-		ft_error();
+		ft_putendl_fd("Error: Bad Arguments", 2);
+	return (0);
 }
-
-
-/*
-
-
-	void forkexample()
-{
-    int x = 1;
-
-    if (fork() == 0)
-        printf("Child has x = %d\n", ++x);
-    else
-        printf("Parent has x = %d\n", --x);
-}
-int main()
-{
-    forkexample();
-    return 0;
-}
-
-	(void)argc;
-	(void)argv;
-	for (int i = 0; envp[i] != NULL; i++)
-	{
-		printf("%s\n", envp[i]);
-	}
-
-	void	child_proc(char ** argv, char **envp, int *fd)
-{
-	int filein;
-
-	filein = open(argv[1], O_RDONLY, 0551);
-	if (filein == -1)
-	{
-		perror("Error:");
-		exit(1);
-	}
-	dup2(fd[1], STDOUT_FILENO);
-	dup2(filein, STDIN_FILENO);
-	close(fd[0]);
-	//execute
-}
-*/
